5d35985e180489630812fe7d97edc7eeab7f0012
hart <hkerbel@sympatico.ca>
2019-08-21 00:54:43 -0400

    About to refactor duplicate code in FT.

--- lists/views.py
+++ lists/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home_page(request):
-    return render(request, 'home.html')
+def home_page(request: HttpResponse):
+    # # # if request.method == "POST":
+    # # #     return HttpResponse(request.POST['item_text'])
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
