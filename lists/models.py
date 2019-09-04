5c9a72aaffc9e7560d35f82542bf009f355f811e
hart <hkerbel@sympatico.ca>
2019-09-04 02:01:23 -0400

    new URL + view for adding to existing lists. FT passes :-)

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
 class List(models.Model):
-    pass
+    objects = models.Manager()  ## HACK: Allow VSC intellisense to know about `objects` attribute. Normally added by Django dynamically. article:https://stackoverflow.com/questions/45135263/class-has-no-objects-member
 
     
 class Item(models.Model):
+    objects = models.Manager()  ## HACK: Allow VSC intellisense to know about `objects` attribute. Normally added by Django dynamically. article:https://stackoverflow.com/questions/45135263/class-has-no-objects-member
+
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
