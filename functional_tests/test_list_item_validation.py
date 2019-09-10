0e7fadc426c05b4d908fc200f1ec41d0a1204431
hart <hkerbel@sympatico.ca>
2019-09-10 01:08:54 -0400

    Adjust new list view to do model validation.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.keys import Keys
 from .base import FunctioalTest
 
+import time
 
 class ItemValidationTest(FunctioalTest):
 
@@ -14,7 +15,7 @@ class ItemValidationTest(FunctioalTest):
 		# that list items cannot be blank.
 		self.wait_for(lambda: self.assertEqual(
 			self.browser.find_element_by_css_selector('.has-error').text,
-			"You can't have an empty list item."
+			"You can't have an empty list item"
 		))
 
 		# She tries again with some text for the item, which now works.
@@ -28,7 +29,7 @@ class ItemValidationTest(FunctioalTest):
 		# She receives a similar warning on the list page.
 		self.wait_for(lambda: self.assertEqual(
 			self.browser.find_element_by_css_selector('.has-error').text,
-			"You can't have an empty list item."
+			"You can't have an empty list item"
 		))
 
 		# And she can correct it by filling some text in.
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
 
@@ -9,7 +10,14 @@ def home_page(request: HttpResponse):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST.get('item_text'), list=list_)
+    item = Item.objects.create(text=request.POST.get('item_text'), list=list_)
+    try:        
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {'error': error})
     return redirect(f'/lists/{list_.id}/')
 
 
