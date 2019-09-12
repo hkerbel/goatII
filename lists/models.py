032af230a351b0f48886198c970577e7d5d3b0bd
hart <hkerbel@sympatico.ca>
2019-09-12 01:48:15 -0400

    User get_absolute_url on List model to DRY urls in views. [HJK: FTs still failing.]

--- lists/models.py
+++ lists/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class List(models.Model):
     objects = models.Manager()  ## HACK: Allow VSC intellisense to know about `objects` attribute. Normally added by Django dynamically. article:https://stackoverflow.com/questions/45135263/class-has-no-objects-member
 
-    
+    def get_absolute_url(self):
+        return reverse('view_list', args=[self.id])
+
+
 class Item(models.Model):
     objects = models.Manager()  ## HACK: Allow VSC intellisense to know about `objects` attribute. Normally added by Django dynamically. article:https://stackoverflow.com/questions/45135263/class-has-no-objects-member
 
